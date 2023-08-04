04649a845ac5231db885a871fef9878562c8cd01
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-08-04 10:00:34 +0200

    processing weather data

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 import "./style.css"
 import { getWeather } from "./weather"
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-  (res) => {
-    console.log(res.data)
-  }
-)
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e)
+    alert("Unable getting weather.")
+  })
+
+function renderWeather(current, daily, hourly) {
+  renderCurrentWeather(current),
+    //   renderDailyWeather(daily)
+    //   renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function renderCurrentWeather(current) {
+  setValue("current-temp", current.currentTemp)
+  setValue("current-high", current.highTemp)
+  setValue("current-low", current.lowTemp)
+  setValue("current-fl-high", current.highFeelsLike)
+  setValue("current-fl-low", current.lowFeelsLike)
+  setValue("current-wind", current.windSpeed)
+  setValue("current-precip", current.precip)
+}
